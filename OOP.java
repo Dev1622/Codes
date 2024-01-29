d09185bbd28a14f2827bf31196385b3d5c3e317e
Lenovo <deevsurya14@gmail.com>
2024-01-29 15:53:33 +0530

    OOP code is added

---
+++ OOP.java
@@ -0,0 +1,11 @@
+public class OOP{
+    public static void main(String[] args){
+        Car mycar = new Car();
+        Car mycar1 = new Car();
+        System.out.println(mycar.model);
+        mycar.brake();
+
+        mycar1.run();
+        
+    }
+}
